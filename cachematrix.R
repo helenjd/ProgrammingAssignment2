475226d7ac772f7c91efef6a273b3d971a40b1f1
Helenjd <helenjd@users.noreply.github.com>
2014-09-18 13:15:54 +0100

    Comments added and indentation increased

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,37 @@
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    set <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    get <- function() x
-    setinv <- function(inverse) inv <<- inverse
-    getinv <- function() inv
-    list(set = set, get = get,
-         setinv = setinv,
-         getinv = getinv)
+        #This function creates a "list" containing methods to get 
+        #and set the values of the matrix and its inverse
+        
+        # initialise inv
+        inv <- NULL
+        #define set as a function using superassignment operator
+        #so that x and inv can be used outside this environment
+        
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        
+        get <- function() x
+        #define setinv using superassignment operator
+        setinv <- function(inverse) inv <<- inverse
+        getinv <- function() inv
+        ## Put all "sets" and "gets" into a list and return that
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 cacheSolve <- function(x, ...) {
-    inv <- x$getinv()
-    if(!is.null(inv)) {
-        message("getting cached data")
-        return(inv)
-    }
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                # if the inverse has already been calculated use the value from the cache
+                message("getting cached data")
+                return(inv)
+        }
+    # Need to get the matrix and invert it using "solve"
     to_be_inverted <- x$get()
     inv <- solve(to_be_inverted)
+    # Put inverse into the cache for future use
     x$setinv(inv)
     inv
 }
